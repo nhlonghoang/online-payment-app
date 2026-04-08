edeb8f520e099b438bcc11567a6f8bdaa4a3bc41
nhlonghoang <hlongnguyen2000@gmail.com>
2026-04-08 21:57:10 +0700

    Add Categories table

--- OnlinePaymentApp/Data/ApplicationDbContext.cs
+++ OnlinePaymentApp/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using OnlinePaymentApp.Models;
 
 namespace OnlinePaymentApp.Data
 {
@@ -8,5 +9,7 @@ namespace OnlinePaymentApp.Data
         {
             
         }
+
+        public DbSet<Category> Categories { get; set; }
     }
 }
