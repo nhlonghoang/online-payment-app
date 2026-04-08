d5760cabc4def6ea9d0cccfebbcba52263ba47af
nhlonghoang <hlongnguyen2000@gmail.com>
2026-04-08 22:53:03 +0700

    Load categories from DB to view

--- OnlinePaymentApp/Controllers/CategoryController.cs
+++ OnlinePaymentApp/Controllers/CategoryController.cs
@@ -1,12 +1,17 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using OnlinePaymentApp.Data;
+using OnlinePaymentApp.Models;
 
 namespace OnlinePaymentApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) { _db = db; }
         public IActionResult Index()
         {
-            return View();
+            List<Category> categories = _db.Categories.ToList();
+            return View(categories);
         }
     }
 }
