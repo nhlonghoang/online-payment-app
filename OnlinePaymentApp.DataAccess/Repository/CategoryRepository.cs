31e4d8b95541cc7fc43589f4c35541dee47af65f
nhlonghoang <hlongnguyen2000@gmail.com>
2026-04-15 08:14:23 +0700

    Implement CategoryRepository

---
+++ OnlinePaymentApp.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlinePaymentApp.DataAccess.Repository.IRepository;
+using OnlinePaymentApp.DataAcess.Data;
+using OnlinePaymentApp.Models;
+
+namespace OnlinePaymentApp.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db) {
+        
+            _db = db;
+        }
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(Category obj)
+        {
+            _db.Categories.Update(obj);
+        }
+    }
+}
