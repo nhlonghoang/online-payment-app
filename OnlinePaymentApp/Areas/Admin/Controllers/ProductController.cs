94080d560a1dd92e75733f4d0db7ae621a66453b
nhlonghoang <hlongnguyen2000@gmail.com>
2026-04-17 17:25:14 +0700

    Store upload image

--- OnlinePaymentApp/Areas/Admin/Controllers/ProductController.cs
+++ OnlinePaymentApp/Areas/Admin/Controllers/ProductController.cs
@@ -11,7 +11,11 @@ namespace OnlinePaymentApp.Areas.Admin.Controllers
     public class ProductController : Controller
     {   
         private readonly IUnitOfWork _unitOfWork;
-        public ProductController(IUnitOfWork unitOfWork) { _unitOfWork = unitOfWork; }
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public ProductController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment) {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
         public IActionResult Index()
         {
             List<Product> products = _unitOfWork.ProductRepository.GetAll().ToList();
@@ -50,6 +54,18 @@ namespace OnlinePaymentApp.Areas.Admin.Controllers
         {
             if (ModelState.IsValid)  // Check model validation
             {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null) { 
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string productPath = Path.Combine(wwwRootPath, @"images\product");
+
+                    using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
                 _unitOfWork.ProductRepository.Add(productVM.Product);
                 _unitOfWork.Save();
                 TempData["success"] = "Product created successfully";
