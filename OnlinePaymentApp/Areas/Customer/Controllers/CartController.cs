1febcc73cd3a136dfb565fb4c6a2e9c3efa6a107
nhlonghoang <hlongnguyen2000@gmail.com>
2026-04-22 23:13:56 +0700

    Update quantity in shopping cart

--- OnlinePaymentApp/Areas/Customer/Controllers/CartController.cs
+++ OnlinePaymentApp/Areas/Customer/Controllers/CartController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using OnlinePaymentApp.DataAccess.Repository.IRepository;
 using OnlinePaymentApp.Models;
@@ -7,16 +8,21 @@ using OnlinePaymentApp.Models.ViewModels;
 namespace OnlinePaymentApp.Areas.Customer.Controllers
 {
     [Area("customer")]
+    [Authorize]
     public class CartController : Controller
     {
+
         private readonly IUnitOfWork _unitOfWork;
         public ShoppingCartVM ShoppingCartVM { get; set; }
         public CartController(IUnitOfWork unitOfWork)
         {
             _unitOfWork = unitOfWork;
         }
+
+
         public IActionResult Index()
         {
+
             var claimsIdentity = (ClaimsIdentity)User.Identity;
             var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
 
@@ -35,7 +41,44 @@ namespace OnlinePaymentApp.Areas.Customer.Controllers
             return View(ShoppingCartVM);
         }
 
-        // helper
+
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            if (cartFromDb.Count <= 1)
+            {
+                //remove that from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.Id == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+
+
         private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
             if (shoppingCart.Count <= 50)
