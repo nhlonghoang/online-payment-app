66146c9394298478eff14244763d63c2c183c68c
nhlonghoang <hlongnguyen2000@gmail.com>
2026-04-28 14:58:32 +0700

    Show order list

---
+++ OnlinePaymentApp.Models/ViewModels/OrderVM.cs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace OnlinePaymentApp.Models.ViewModels
+{
+    public class OrderVM
+    {
+        public OrderHeader OrderHeader { get; set; }
+        public IEnumerable<OrderDetail> OrderDetail { get; set; }
+    }
+}
---
+++ OnlinePaymentApp/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using OnlinePaymentApp.DataAccess.Repository.IRepository;
+using OnlinePaymentApp.DataAcess.Data;
+using OnlinePaymentApp.Models;
+using OnlinePaymentApp.Models.ViewModels;
+using OnlinePaymentApp.Utility;
+
+namespace OnlinePaymentApp.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class OrderController : Controller
+    {
+
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<OrderHeader> OrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+            return View(OrderHeaders);
+        }
+
+
+    }
+}
