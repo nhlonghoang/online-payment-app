51ce72e49b09dcd6000e119e546ed2ad0091b993
nhlonghoang <hlongnguyen2000@gmail.com>
2026-04-29 13:22:48 +0700

    Create user specify role using ADMIN PORTAL

--- OnlinePaymentApp/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ OnlinePaymentApp/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -210,7 +210,14 @@ namespace OnlinePaymentApp.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin)) // if role admin -> only show notification without logout
+                        {
+                            TempData["success"] = "New User Created Successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
